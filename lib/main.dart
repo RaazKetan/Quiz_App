1b3fa2bd111052c79cc7d7a2dde0def99b17eafa
Raaz <21ketanraaz@gmail.com>
2023-03-27 13:47:37 +0530

    +button, questions

--- lib/main.dart
+++ lib/main.dart
@@ -8,12 +8,33 @@ class MyApp extends StatelessWidget {
 
   Widget build (BuildContext context)
   {
+    var question = [
+      'Whats\'s your favorite color?',
+      'Whats\'s your favorite color?',
+      'Whats\'s your favorite color?',
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Center(child: Text('Quiz App')),
         ),
-        body: Text('This is my default text'),
+        body: Column(
+          children: [
+            Text('The question!'),
+            ElevatedButton(
+              onPressed: null,
+              child: Text('Answer 2'),
+            ),
+            ElevatedButton(
+              onPressed: null,
+              child: Text('Answer 2'),
+            ),
+            ElevatedButton(
+              onPressed: null,
+              child: Text('Answer 2'),
+            ),
+          ],
+        )
       ),
     );
 
